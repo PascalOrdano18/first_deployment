3c8e01efd68df31629d514f30c10d974d2d13852
Pascal Ordano <pascalordanodev@gmail.com>
2022-06-24 02:30:45 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@
 function App() {
   return (
         <div>
-          <div className="w-40 text-black rounded bg-green-50 mt-40 mx-auto text-center">Hey!</div>
-          <div className="w-40 text-black rounded bg-green-100 mt-10 mx-auto text-center">This</div>
-          <div className="w-40 text-black rounded bg-green-200 mt-10 mx-auto text-center">Is</div>
-          <div className="w-40 text-black rounded bg-green-300 mt-10 mx-auto text-center">Just</div>
-          <div className="w-40 text-black rounded bg-green-400 mt-10 mx-auto text-center">A</div>
-          <div className="w-40 text-black rounded bg-green-500 mt-10 mx-auto text-center">Simple</div>
-          <div className="w-40 text-black rounded bg-green-600 mt-10 mx-auto text-center">React</div>
-          <div className="w-40 text-black rounded bg-green-700 mt-10 mx-auto text-center">App</div>
-          <div className="w-40 text-black rounded bg-green-800 mt-10 mx-auto text-center">PASCAL</div>
-          <div className="w-40 text-black rounded bg-green-900 mt-10 mx-auto text-center">ORDANO</div>
+          <div className="w-40 h-8 text-black rounded bg-green-50 mt-40 mx-auto text-center  hover:scale-125 transition-all hover:cursor-pointer" >Hey!</div>
+          <div className="w-40 h-8 text-black rounded bg-green-100 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">This</div>
+          <div className="w-40 h-8 text-black rounded bg-green-200 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">Is</div>
+          <div className="w-40 h-8 text-black rounded bg-green-300 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">Just</div>
+          <div className="w-40 h-8 text-black rounded bg-green-400 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">A</div>
+          <div className="w-40 h-8 text-black rounded bg-green-500 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">Simple</div>
+          <div className="w-40 h-8 text-black rounded bg-green-600 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">React</div>
+          <div className="w-40 h-8 text-black rounded bg-green-700 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">App</div>
+          <div className="w-40 h-8 text-black rounded bg-green-800 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">PASCAL</div>
+          <div className="w-40 h-8 text-black rounded bg-green-900 mt-10 mx-auto text-center hover:scale-125 transition-all hover:cursor-pointer">ORDANO</div>
         </div>
   );
 }
