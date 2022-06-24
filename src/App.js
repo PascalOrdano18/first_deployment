f58beb131e93ec1214673f6e7502379f3f2b73d3
Pascal Ordano <pascalordanodev@gmail.com>
2022-06-24 02:11:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+        <div>
+          <div className="w-40 text-black rounded bg-green-50 mt-40 mx-auto text-center">Hey!</div>
+          <div className="w-40 text-black rounded bg-green-100 mt-10 mx-auto text-center">This</div>
+          <div className="w-40 text-black rounded bg-green-200 mt-10 mx-auto text-center">Is</div>
+          <div className="w-40 text-black rounded bg-green-300 mt-10 mx-auto text-center">Just</div>
+          <div className="w-40 text-black rounded bg-green-400 mt-10 mx-auto text-center">A</div>
+          <div className="w-40 text-black rounded bg-green-500 mt-10 mx-auto text-center">Simple</div>
+          <div className="w-40 text-black rounded bg-green-600 mt-10 mx-auto text-center">React</div>
+          <div className="w-40 text-black rounded bg-green-700 mt-10 mx-auto text-center">App</div>
+          <div className="w-40 text-black rounded bg-green-800 mt-10 mx-auto text-center">PASCAL</div>
+          <div className="w-40 text-black rounded bg-green-900 mt-10 mx-auto text-center">ORDANO</div>
+        </div>
+  );
+}
+
+export default App;
